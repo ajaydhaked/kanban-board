e8ab2dee85a741be6b68b36163eed51a458bad9b
Ajay Kumar Dhakar <ajaydhaker2002@kgpian.iitkgp.ac.in>
2024-12-04 12:45:42 +0530

    v0.2

--- src/components/data/PriorityIcons.js
+++ src/components/data/PriorityIcons.js
@@ -5,7 +5,7 @@ const PriorityIcons = {
     1: <img src="/icons/LowPriority.svg" alt="Low" />,
     2: <img src="/icons/mediumPriority.svg" alt="Low" />,
     3: <img src="/icons/HighPriority.svg" alt="Low" />,
-    4: <img src="/icons/UrgentPriority(2).svg" alt="Low" />
+    4: <img src="/icons/UrgentPriority2.svg" alt="Low" />
 }
 
 export default PriorityIcons
